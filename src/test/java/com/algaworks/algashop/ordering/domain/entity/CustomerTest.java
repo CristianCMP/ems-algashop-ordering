4f13492f4dc8b93f6a341c66ae60cb26ec7d82bf
Cristian Puhl <cristian.puhl@docile.com.br>
2025-07-27 09:58:34 -0300

    Add business rule with unit test

--- src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/CustomerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CustomerTest {
 
     @Test
@@ -47,5 +49,30 @@ class CustomerTest {
                 .isThrownBy(() -> {
                     customer.changeEmail("invalid");
                 });
+    }
+
+    @Test
+    @DisplayName("Should anonymize Customer data when an unarchived Customer is archived")
+    void given_unarquivedCustomer_whenArquived_shouldAnonymize() {
+        Customer customer = new Customer(
+                IdGenerator.generateTimeBasedUUID(),
+                "Cristian Puhl",
+                LocalDate.of(1998, 1, 29),
+                "cristian.puhl@test.com",
+                "123-456-789",
+                "123-45-6789",
+                true,
+                OffsetDateTime.now()
+        );
+
+        customer.archive();
+
+        Assertions.assertWith(customer,
+                c -> assertThat(c.fullName()).isEqualTo("Anonymous"),
+                c -> assertThat(c.email()).isNotEqualTo("cristian.puhl@test.com"),
+                c -> assertThat(c.phone()).isEqualTo("000-000-0000"),
+                c -> assertThat(c.document()).isEqualTo("000-00-0000"),
+                c -> assertThat(c.birthDate()).isNull()
+        );
     }
 }
