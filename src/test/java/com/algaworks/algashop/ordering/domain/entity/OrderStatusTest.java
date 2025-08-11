992ac36217566db35ed813db3bac49a01b6100f9
Cristian Puhl <cristian.puhl@docile.com.br>
2025-08-11 20:17:02 -0300

    - Controller updated to change status

---
+++ src/test/java/com/algaworks/algashop/ordering/domain/entity/OrderStatusTest.java
@@ -0,0 +1,19 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderStatusTest {
+
+    @Test
+    public void canChangeTo() {
+        assertThat(OrderStatus.DRAFT.canChangeTo(OrderStatus.PLACED)).isTrue();
+        assertThat(OrderStatus.DRAFT.canChangeTo(OrderStatus.CANCELED)).isTrue();
+        assertThat(OrderStatus.PAID.canChangeTo(OrderStatus.DRAFT)).isFalse();
+    }
+    @Test
+    public void canNotChangeTo() {
+        assertThat(OrderStatus.PLACED.canNotChangeTo(OrderStatus.DRAFT)).isTrue();
+    }
+}
