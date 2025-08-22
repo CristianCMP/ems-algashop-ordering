09addcf9f87e58c50218a717025cb5f8b73ba125
Cristian Puhl <cristian.puhl@docile.com.br>
2025-08-22 08:20:49 -0300

    - Protecting against unauthorized version changes, using java reflection

--- src/main/java/com/algaworks/algashop/ordering/domain/model/entity/Order.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/entity/Order.java
@@ -309,7 +309,7 @@ public class Order implements AggregateRoot<OrderId> {
         this.id = id;
     }
 
-    public void setVersion(Long version) {
+    private void setVersion(Long version) {
         this.version = version;
     }
 
