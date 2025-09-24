75499ac3dd353f2bfb75e8cb8e109e0e5350d2b4
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-09-24 00:11:57 -0300

    - Modules applied to the infrastructure layer

---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/order/ShippingEmbeddable.java
@@ -0,0 +1,29 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.order;
+
+import com.algaworks.algashop.ordering.infrastructure.persistence.commons.AddressEmbeddable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class ShippingEmbeddable {
+    @Column(name = "shipping_cost")
+    private BigDecimal cost;
+    @Column(name = "shipping_expected_date")
+    private LocalDate expectedDate;
+    @Embedded
+    private AddressEmbeddable address;
+    @Embedded
+    private RecipientEmbeddable recipient;
+}
---
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/CustomerPersistenceEntityRepositoryIT.java
@@ -0,0 +1,55 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.customer;
+
+import com.algaworks.algashop.ordering.infrastructure.persistence.SpringDataAuditingConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest // load the full Spring application context and all beans.
+//@Transactional  // @DataJpaTest has @Transactional inside by default.
+@DataJpaTest  // configuration only JPA.
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)  // do not change the database's configure.
+@Import(SpringDataAuditingConfig.class)
+class CustomerPersistenceEntityRepositoryIT {
+
+    private final CustomerPersistenceEntityRepository customerPersistenceEntityRepository;
+
+    @Autowired
+    public CustomerPersistenceEntityRepositoryIT(CustomerPersistenceEntityRepository customerPersistenceEntityRepository) {
+        this.customerPersistenceEntityRepository = customerPersistenceEntityRepository;
+    }
+
+    @Test
+    public void shouldPersist() {
+        CustomerPersistenceEntity entity = CustomerPersistenceEntityTestDataBuilder.aCustomer().build();
+
+        customerPersistenceEntityRepository.saveAndFlush(entity);
+
+        assertThat(customerPersistenceEntityRepository.existsById(entity.getId())).isTrue();
+
+        CustomerPersistenceEntity savedEntity = customerPersistenceEntityRepository.findById(entity.getId()).orElseThrow();
+
+        assertThat(entity).isEqualTo(savedEntity);
+    }
+
+    @Test
+    public void shouldCount() {
+        long customersCoung = customerPersistenceEntityRepository.count();
+        assertThat(customersCoung).isZero();
+    }
+
+    @Test
+    public void shouldSetAuditingValues() {
+        CustomerPersistenceEntity entity = CustomerPersistenceEntityTestDataBuilder.aCustomer().build();
+
+        entity = customerPersistenceEntityRepository.saveAndFlush(entity);
+
+        assertThat(entity.getCreatedByUserId()).isNotNull();
+        assertThat(entity.getLastModifiedAt()).isNotNull();
+        assertThat(entity.getLastModifiedByUserId()).isNotNull();
+    }
+}
