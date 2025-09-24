5a8c4bf33b4bc9ed90d402c6dda6d5fb423bef6b
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-09-23 21:23:19 -0300

    - Challenge - Implement Domain Service for ShoppingCart

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/exception/CustomerAlreadyHaveShoppingCartException.java
@@ -0,0 +1,6 @@
+package com.algaworks.algashop.ordering.domain.model.exception;
+
+public class CustomerAlreadyHaveShoppingCartException extends DomainException {
+    public CustomerAlreadyHaveShoppingCartException() {
+    }
+}
---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/exception/CustomerNotFoundException.java
@@ -0,0 +1,6 @@
+package com.algaworks.algashop.ordering.domain.model.exception;
+
+public class CustomerNotFoundException extends DomainException {
+    public CustomerNotFoundException() {
+    }
+}
