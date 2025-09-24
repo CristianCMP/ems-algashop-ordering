320a56c80d6394b255276b3a405ec262b79b3eff
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-09-23 21:11:09 -0300

    - Challenge - Implement Domain Service for Checkout

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/service/CheckoutService.java
@@ -0,0 +1,41 @@
+package com.algaworks.algashop.ordering.domain.model.service;
+
+import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.entity.PaymentMethod;
+import com.algaworks.algashop.ordering.domain.model.entity.ShoppingCart;
+import com.algaworks.algashop.ordering.domain.model.exception.ShoppingCartCantProceedToCheckoutException;
+import com.algaworks.algashop.ordering.domain.model.utility.DomainService;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Billing;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Product;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Shipping;
+import lombok.RequiredArgsConstructor;
+
+@DomainService
+@RequiredArgsConstructor
+public class CheckoutService {
+
+    public Order checkout(ShoppingCart shoppingCart, Billing billing, Shipping shipping, PaymentMethod paymentMethod) {
+        if (shoppingCart.containsUnavailableItems() || shoppingCart.isEmpty()) {
+            throw new ShoppingCartCantProceedToCheckoutException();
+        }
+
+        Order order = Order.draft(shoppingCart.customerId());
+        order.changeBilling(billing);
+        order.changeShipping(shipping);
+        order.changePaymentMethod(paymentMethod);
+
+        addOrderItem(shoppingCart, order);
+
+        order.place();
+        shoppingCart.empty();
+
+        return order;
+    }
+
+    private void addOrderItem(ShoppingCart shoppingCart, Order order) {
+        shoppingCart.items().forEach(item -> {
+            Product product = new Product(item.productId(), item.name(), item.price(), item.isAvailable());
+            order.addItem(product, item.quantity());
+        });
+    }
+}
