bee3861de99c74f994378ae129c973e6879c8d23
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-09-27 16:54:34 -0300

    - Reacting to events

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/listener/customer/CustomerEventListener.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/listener/customer/CustomerEventListener.java
@@ -1,23 +1,25 @@
 package com.algaworks.algashop.ordering.infrastructure.listener.customer;
 
+import com.algaworks.algashop.ordering.application.customer.notification.CustomerNotificationService;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerArchivedEvent;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerRegisteredEvent;
+import com.algaworks.algashop.ordering.domain.model.customer.CustomerRegistrationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class CustomerEventListener {
+
+    private final CustomerNotificationService customerNotificationService;
 
     @EventListener
     public void listen(CustomerRegisteredEvent event){
         log.info("CustomerEventListener listen 1");
-    }
-
-    @EventListener
-    public void listenSecondary(CustomerRegisteredEvent event){
-        log.info("CustomerEventListener listen 2");
+        customerNotificationService.notifyNewRegistration(event.customerId().value());
     }
 
     @EventListener
